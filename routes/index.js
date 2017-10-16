1c6a2c99d51f485d68351778b7b52c3eb92d5f08
jasenpan-infotrack <jasen.zheng@infotrack.com.au>
2017-10-17 00:53:29 +1100

    start parse response

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,26 @@
+const axios = require("axios");
+var parser = require('xml2json');
+
+const URI = 'https://api.flickr.com/services/feeds/photos_public.gne'
+
+const getEntry = feedObj => feedObj.entry;
+
+const mapEntry = records => {
+  return records.map(record => {
+    return {
+      title: re
+    }
+  });
+}
+
 module.exports = app => {
-  app.get("/api/feed", (req, res) => {
-    return res.send("hello world");
+  app.get("/api/feed", async (req, res) => {
+    try {
+      const resp = await axios.get(URI);
+      const jsonResp = parser.toJson(resp.data);
+      return res.json(JSON.parse(jsonResp));
+    } catch(error) {
+      res.send(error);
+    }
   });
 };
